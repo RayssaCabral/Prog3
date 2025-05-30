dc22889f772ad9bbddfeb901d7fbb827545e9ad2
RayssaCabral <126641778+RayssaCabral@users.noreply.github.com>
2025-05-29 21:00:38 -0300

    Add files via upload

---
+++ Herança em Java/questao_1/Estagiario.java
@@ -0,0 +1,10 @@
+class Estagiario extends Funcionario {
+    public Estagiario(String nome, double salarioBase) {
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() * 0.80;
+    }
+}
---
+++ Herança em Java/questao_1/Gerente.java
@@ -0,0 +1,10 @@
+class Gerente extends Funcionario {
+    public Gerente(String nome, double salarioBase) {
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() * 1.10;
+    }
+}
