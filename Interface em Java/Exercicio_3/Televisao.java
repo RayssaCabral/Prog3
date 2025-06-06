04b7b5655639c87f43ce5c38a59f3c35ef0fb779
RayssaCabral <126641778+RayssaCabral@users.noreply.github.com>
2025-06-05 22:58:47 -0300

    Add files via upload

---
+++ Interface em Java/Exercicio_3/Televisao.java
@@ -0,0 +1,11 @@
+class Televisao implements DispositivoEletronico {
+    @Override
+    public void ligar() {
+        System.out.println("TV ligada.");
+    }
+
+    @Override
+    public void desligar() {
+        System.out.println("TV desligada.");
+    }
+}
